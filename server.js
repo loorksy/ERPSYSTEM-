5099176da58a02e8c553c6e3d49c87a5230d8139
loorksy <loorksy@gmail.com>
2026-03-19 04:05:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -114,8 +114,8 @@ app.use(helmet({
   },
 }));
 app.use(compression());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
